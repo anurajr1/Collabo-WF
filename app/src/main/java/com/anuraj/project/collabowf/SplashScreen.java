653c4437f9379a01bd97b634cc6dcb63db442756
Anuraj R <anuraj.r@sap.com>
2019-03-22 11:20:08 +0530

    Code Cleanup | Splashscreen

--- app/src/main/java/com/anuraj/project/collabowf/SplashScreen.java
+++ app/src/main/java/com/anuraj/project/collabowf/SplashScreen.java
@@ -6,25 +6,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
 	// Splash screen timer
     private static int SPLASH_TIME_OUT = 1500;
-    //ImageView imageAnimation;
-   // Animation anim;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
-        //imageAnimation = (ImageView) findViewById(R.id.imageView1);
-        //anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_bounce);
-        //imageAnimation.setVisibility(View.VISIBLE);
-        //imageAnimation.startAnimation(anim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -32,7 +22,6 @@ public class SplashScreen extends Activity {
                 // Start  app main activity
                 Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
- 
                 // close this activity
                 finish();
             }
