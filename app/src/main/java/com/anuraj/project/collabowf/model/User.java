1a523a58b1f56138106d8ea57cee986e835b15fd
Anuraj R <anuraj.r@sap.com>
2019-03-22 17:34:44 +0530

    Getters and setters added in users

--- app/src/main/java/com/anuraj/project/collabowf/model/User.java
+++ app/src/main/java/com/anuraj/project/collabowf/model/User.java
@@ -13,6 +13,38 @@ public class User {
     public User() {
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
     public User(String id, String name, String password, String domain) {
         this.id = id;
         this.name = name;
