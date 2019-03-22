b56d41086b760e0ec28e8d1c47533f6eb631433f
Anuraj R <anuraj.r@sap.com>
2019-03-22 11:16:47 +0530

    Login layout and Activity

---
+++ app/src/main/java/com/anuraj/project/collabowf/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.anuraj.project.collabowf;
+/**
+ * Created by Anuraj R(i321994) a4anurajr@gmail.com
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginlayout);
+    }
+}
