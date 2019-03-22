2698c2b2934c60b02ad84b99e53d68a1b8998c27
Anuraj R <anuraj.r@sap.com>
2019-03-22 17:55:52 +0530

    login activity

--- app/src/main/java/com/anuraj/project/collabowf/LoginActivity.java
+++ app/src/main/java/com/anuraj/project/collabowf/LoginActivity.java
@@ -3,13 +3,87 @@ package com.anuraj.project.collabowf;
  * Created by Anuraj R(i321994) a4anurajr@gmail.com
  */
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.anuraj.project.collabowf.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginActivity extends Activity {
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
+    User users;
+    private EditText inputId, inputPassword;
+    private Button btnLogin;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginlayout);
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+
+
+        inputId = (EditText) findViewById(R.id.input_employeeid);
+        inputPassword = (EditText) findViewById(R.id.input_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String password = inputPassword.getText().toString();
+                final String id = inputId.getText().toString();
+                if(!(password.isEmpty()) && !(id.isEmpty())){
+
+                    // Read from the database
+                    mFirebaseDatabase.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for (DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                                users = userSnapshot.getValue(User.class);
+                                if((id.equalsIgnoreCase(users.id)) && (password.equalsIgnoreCase(users.password))){
+                                    Intent i = new Intent(LoginActivity.this, SplashScreen.class);
+                                    startActivity(i);
+                                    finish();
+                                }
+                            }
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(getBaseContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
