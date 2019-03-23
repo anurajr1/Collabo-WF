e05ef24ee6454a3e026309b16cc2d766d3ba38da
Anuraj R <anuraj.r@sap.com>
2019-03-23 11:04:24 +0530

    code clean up in login activity

--- app/src/main/java/com/anuraj/project/collabowf/LoginActivity.java
+++ app/src/main/java/com/anuraj/project/collabowf/LoginActivity.java
@@ -11,15 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.anuraj.project.collabowf.model.User;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 
 import static android.content.ContentValues.TAG;
 
@@ -99,8 +96,6 @@ public class LoginActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-
-        //pref.getString("employeeId", null);
 
         if ((pref.getString("employeeId", null))!= null) {
             Intent intent = new Intent(this, SplashScreen.class);
