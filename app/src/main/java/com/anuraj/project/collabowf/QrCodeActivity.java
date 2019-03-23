5dcb79439af60a8301266b1557b45cea38257588
Anuraj R <anuraj.r@sap.com>
2019-03-23 12:39:20 +0530

    QR code activity

---
+++ app/src/main/java/com/anuraj/project/collabowf/QrCodeActivity.java
@@ -0,0 +1,17 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 22/3/19 11:19 AM
+ *
+ */
+package com.anuraj.project.collabowf;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QrCodeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrcodeloginlayout);
+    }
+}
