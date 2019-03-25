6fd57d3f4affcb2a62c26c277e4376d1a9b9c4ab
Anuraj R <anuraj.r@sap.com>
2019-03-25 14:28:08 +0530

    Added changes to handle operator activity

--- app/src/main/java/com/anuraj/project/collabowf/SplashScreen.java
+++ app/src/main/java/com/anuraj/project/collabowf/SplashScreen.java
@@ -10,18 +10,29 @@ import android.os.Handler;
 public class SplashScreen extends Activity {
 	// Splash screen timer
     private static int SPLASH_TIME_OUT = 1500;
+    private String employeeDomain;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+
+        Intent intent = getIntent();
+        employeeDomain = intent.getExtras().getString("employeeDomain");
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                // Start  app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
+                if(employeeDomain.equalsIgnoreCase("Supervisor")) {
+                    // Start Supervisor main activity
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                }else{
+                    // Start operator main activity
+                    Intent i = new Intent(SplashScreen.this, OperatorMainActivity.class);
+                    startActivity(i);
+                }
                 // close this activity
                 finish();
             }
