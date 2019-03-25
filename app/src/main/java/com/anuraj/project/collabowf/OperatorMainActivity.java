a0d44f7947138e6ade70510cf5845aeb5216f907
Anuraj R <anuraj.r@sap.com>
2019-03-25 14:27:38 +0530

    Operator Activity added

---
+++ app/src/main/java/com/anuraj/project/collabowf/OperatorMainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 25/3/19 2:19 PM
+ *
+ */
+
+package com.anuraj.project.collabowf;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OperatorMainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.operator_main_activity);
+    }
+}
