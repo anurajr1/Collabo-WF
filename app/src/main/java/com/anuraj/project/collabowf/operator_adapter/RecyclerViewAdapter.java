ef85c7ccd09863832c9c58e0a0e0173333623bc1
Anuraj R <anuraj.r@sap.com>
2019-04-08 14:10:57 +0530

    handle of on click in recyclerview adapter

--- app/src/main/java/com/anuraj/project/collabowf/operator_adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/anuraj/project/collabowf/operator_adapter/RecyclerViewAdapter.java
@@ -8,15 +8,20 @@
 package com.anuraj.project.collabowf.operator_adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.anuraj.project.collabowf.LoginActivity;
 import com.anuraj.project.collabowf.R;
+import com.anuraj.project.collabowf.SplashScreen;
 import com.anuraj.project.collabowf.image_util.ImageLoader;
+import com.anuraj.project.collabowf.operator_activity.TeamMoreDetailsActivity;
 
 import java.util.List;
 
@@ -57,6 +62,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         imageLoader.DisplayImage(teamDetails.getPropic(), holder.propic);
 
+
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // Get the intent to launch the specified
+                    TextView oper = view.findViewById(R.id.operator_id);
+                    oper.getText();
+                    Intent i = new Intent(context, TeamMoreDetailsActivity.class);
+                    i.putExtra("OperatorId",oper.getText());
+                    context.startActivity(i);
+            }
+        });
 
 
     }
