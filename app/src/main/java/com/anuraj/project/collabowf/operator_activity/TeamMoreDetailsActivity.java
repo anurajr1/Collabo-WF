79b88b21679e20d1846b80059f9a7cbe1ce7f4a6
Anuraj R <anuraj.r@sap.com>
2019-04-09 11:24:16 +0530

    Team more details commit for layout changes

--- app/src/main/java/com/anuraj/project/collabowf/operator_activity/TeamMoreDetailsActivity.java
+++ app/src/main/java/com/anuraj/project/collabowf/operator_activity/TeamMoreDetailsActivity.java
@@ -5,44 +5,22 @@
  *
  */
 
-/*
- * *
- *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
- *  * Last modified 25/3/19 2:21 PM
- *
- */
-
 package com.anuraj.project.collabowf.operator_activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
 
 import com.anuraj.project.collabowf.R;
 
-import static com.anuraj.project.collabowf.utils.AppConstants.RALEWAY_REGULAR;
-import static com.anuraj.project.collabowf.utils.AppConstants.RALEWAY_SEMI_BOLD;
-
 public class TeamMoreDetailsActivity extends AppCompatActivity {
-    Typeface ralewayRegular,ralewaySemiBold;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.addevent_operator_layout);
+        setContentView(R.layout.teammoredetail_layout);
         //setting the close icon and title name
-        setupToolBar();
+        //setupToolBar();
 
-        ralewayRegular = Typeface.createFromAsset(getApplicationContext().getAssets(),
-                RALEWAY_REGULAR);
-        ralewaySemiBold = Typeface.createFromAsset(getApplicationContext().getAssets(),
-                RALEWAY_SEMI_BOLD);
-
-        TextView dateTitle = findViewById(R.id.textView6);
-        dateTitle.setTypeface(ralewayRegular);
-        TextView dateTitleValue = findViewById(R.id.textView7);
-        dateTitleValue.setTypeface(ralewaySemiBold);
     }
 
 
@@ -50,7 +28,7 @@ public class TeamMoreDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar == null) return;
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Add Event");
+        getSupportActionBar().setTitle("More detail");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
     }
