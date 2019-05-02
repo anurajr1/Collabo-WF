f79012c17978490c636dde766533db50d209826c
Anuraj R <anuraj.r@sap.com>
2019-05-02 14:20:37 +0530

    AlertFragment added to handle alert screen

---
+++ app/src/main/java/com/anuraj/project/collabowf/fragment/AlertFragment.java
@@ -0,0 +1,91 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 2/5/19 11:25 AM
+ *
+ */
+
+package com.anuraj.project.collabowf.fragment;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anuraj.project.collabowf.R;
+import com.anuraj.project.collabowf.operator_adapter.RecyclerViewAdapter;
+import com.anuraj.project.collabowf.operator_adapter.TeamDetails;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlertFragment extends Fragment {
+    DatabaseReference databaseReference;
+
+    ProgressDialog progressDialog;
+
+    List<TeamDetails> list = new ArrayList<>();
+
+    RecyclerView recyclerView;
+
+    RecyclerView.Adapter adapter ;
+    public AlertFragment(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.teamfragment, container, false);
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        progressDialog = new ProgressDialog(getContext());
+
+        progressDialog.setMessage("Loading Data");
+
+        progressDialog.show();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    TeamDetails teamDetails = dataSnapshot.getValue(TeamDetails.class);
+                    //filter to filter only operator list
+                    if(teamDetails.getDomain().equalsIgnoreCase("Operator")){
+                        list.add(teamDetails);
+                    }
+                }
+
+                adapter = new RecyclerViewAdapter(getActivity(), list);
+
+                recyclerView.setAdapter(adapter);
+
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                progressDialog.dismiss();
+
+            }
+        });
+
+        return rootView;
+    }
+}
