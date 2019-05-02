b74fdea21c0f96d6a4ec80003066acb9f52a0923
Anuraj R <anuraj.r@sap.com>
2019-05-02 14:28:11 +0530

    removed unwanted code from alert fragment

--- app/src/main/java/com/anuraj/project/collabowf/fragment/AlertFragment.java
+++ app/src/main/java/com/anuraj/project/collabowf/fragment/AlertFragment.java
@@ -10,45 +10,26 @@ package com.anuraj.project.collabowf.fragment;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.anuraj.project.collabowf.R;
-import com.anuraj.project.collabowf.operator_adapter.RecyclerViewAdapter;
-import com.anuraj.project.collabowf.operator_adapter.TeamDetails;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AlertFragment extends Fragment {
     DatabaseReference databaseReference;
 
     ProgressDialog progressDialog;
 
-    List<TeamDetails> list = new ArrayList<>();
-
-    RecyclerView recyclerView;
-
-    RecyclerView.Adapter adapter ;
     public AlertFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.teamfragment, container, false);
-
-        recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
-
-        recyclerView.setHasFixedSize(true);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         progressDialog = new ProgressDialog(getContext());
 
@@ -64,17 +45,8 @@ public class AlertFragment extends Fragment {
 
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
 
-                    TeamDetails teamDetails = dataSnapshot.getValue(TeamDetails.class);
-                    //filter to filter only operator list
-                    if(teamDetails.getDomain().equalsIgnoreCase("Operator")){
-                        list.add(teamDetails);
-                    }
+
                 }
-
-                adapter = new RecyclerViewAdapter(getActivity(), list);
-
-                recyclerView.setAdapter(adapter);
-
                 progressDialog.dismiss();
             }
 
