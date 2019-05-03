b258c978b64739128e7006f5997964eea44ecfb9
Anuraj R <anuraj.r@sap.com>
2019-05-03 15:22:15 +0530

    Events

---
+++ app/src/main/java/com/anuraj/project/collabowf/SwipeableLayout/Events.java
@@ -0,0 +1,29 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 3/5/19 11:34 AM
+ *
+ */
+
+package com.anuraj.project.collabowf.SwipeableLayout;
+
+public class Events {
+    private String eventId;
+    private String eventName;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+}
