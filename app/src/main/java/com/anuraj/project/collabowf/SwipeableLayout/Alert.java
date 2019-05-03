522761eb531f3b8325d042292a0319449654c1db
Anuraj R <anuraj.r@sap.com>
2019-05-03 11:55:51 +0530

    Alert Object class added

---
+++ app/src/main/java/com/anuraj/project/collabowf/SwipeableLayout/Alert.java
@@ -0,0 +1,55 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 2/5/19 6:28 PM
+ *
+ */
+
+// Player.java
+package com.anuraj.project.collabowf.SwipeableLayout;
+
+public class Alert {
+
+    private String name, nationality, club;
+    private Integer rating, age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
