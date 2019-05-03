19aef42fa51b1329792bd50df343cdc783272ae0
Anuraj R <anuraj.r@sap.com>
2019-05-03 15:23:24 +0530

    Event dates class

---
+++ app/src/main/java/com/anuraj/project/collabowf/SwipeableLayout/EventDates.java
@@ -0,0 +1,31 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 3/5/19 11:34 AM
+ *
+ */
+
+package com.anuraj.project.collabowf.SwipeableLayout;
+
+import java.util.ArrayList;
+
+public class EventDates {
+    private String date;
+    private ArrayList<Events> eventsArrayList;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public ArrayList<Events> getEventsArrayList() {
+        return eventsArrayList;
+    }
+
+    public void setEventsArrayList(ArrayList<Events> eventsArrayList) {
+        this.eventsArrayList = eventsArrayList;
+    }
+}
