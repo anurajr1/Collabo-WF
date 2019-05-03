d19b69e695d3d27f9d5c88609ca4d100861d2ee1
Anuraj R <anuraj.r@sap.com>
2019-05-03 11:57:47 +0530

    Swipe controller Actions for Recyclerview - Swipeable Alert

---
+++ app/src/main/java/com/anuraj/project/collabowf/SwipeableLayout/SwipeControllerActions.java
@@ -0,0 +1,16 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 1/7/17 10:04 AM
+ *
+ */
+
+package com.anuraj.project.collabowf.SwipeableLayout;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
