46cfa6fde857108f702ae14048b0a0498f419ab5
Anuraj R <anuraj.r@sap.com>
2019-05-03 11:49:08 +0530

    Alert Demo added | Need to remove

--- app/src/main/java/com/anuraj/project/collabowf/fragment/AlertFragment.java
+++ app/src/main/java/com/anuraj/project/collabowf/fragment/AlertFragment.java
@@ -8,28 +8,42 @@
 package com.anuraj.project.collabowf.fragment;
 
 import android.app.ProgressDialog;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.anuraj.project.collabowf.R;
+import com.anuraj.project.collabowf.SwipeableLayout.Player;
+import com.anuraj.project.collabowf.SwipeableLayout.PlayersDataAdapter;
+import com.anuraj.project.collabowf.SwipeableLayout.SwipeController;
+import com.anuraj.project.collabowf.SwipeableLayout.SwipeControllerActions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlertFragment extends Fragment {
     DatabaseReference databaseReference;
 
     ProgressDialog progressDialog;
+    private PlayersDataAdapter mAdapter;
+    SwipeController swipeController = null;
+    View rootView;
 
     public AlertFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.alertfragment, container, false);
+        rootView = inflater.inflate(R.layout.alertfragment, container, false);
 
         progressDialog = new ProgressDialog(getContext());
 
@@ -58,6 +72,48 @@ public class AlertFragment extends Fragment {
             }
         });
 
+        setPlayersDataAdapter();
+        setupRecyclerView();
+
         return rootView;
+    }
+    private void setPlayersDataAdapter() {
+        List<Player> players = new ArrayList<>();
+
+        Player player = new Player();
+        player.setName("Anu");
+        player.setNationality("Indian");
+        player.setClub("Mexico");
+        player.setRating(90);
+        player.setAge(25);
+        players.add(player);
+
+        mAdapter = new PlayersDataAdapter(players);
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView)rootView.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(mAdapter);
+
+        swipeController = new SwipeController(new SwipeControllerActions() {
+            @Override
+            public void onRightClicked(int position) {
+                mAdapter.players.remove(position);
+                mAdapter.notifyItemRemoved(position);
+                mAdapter.notifyItemRangeChanged(position, mAdapter.getItemCount());
+            }
+        });
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeController);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                swipeController.onDraw(c);
+            }
+        });
     }
 }
