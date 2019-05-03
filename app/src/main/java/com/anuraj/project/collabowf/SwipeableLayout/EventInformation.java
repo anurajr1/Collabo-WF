7d1a4fd3af616b87e9d7fdbb2cc8e0d47e1a692f
Anuraj R <anuraj.r@sap.com>
2019-05-03 15:23:11 +0530

    Event information class

---
+++ app/src/main/java/com/anuraj/project/collabowf/SwipeableLayout/EventInformation.java
@@ -0,0 +1,23 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 3/5/19 11:34 AM
+ *
+ */
+
+package com.anuraj.project.collabowf.SwipeableLayout;
+
+import java.util.ArrayList;
+
+public class EventInformation {
+
+    private ArrayList<EventDates> eventsDatesList = new ArrayList<>();
+
+    public ArrayList<EventDates> getEventsDatesList() {
+        return eventsDatesList;
+    }
+
+    public void setEventsDatesList(ArrayList<EventDates> eventsDatesList) {
+        this.eventsDatesList = eventsDatesList;
+    }
+}
