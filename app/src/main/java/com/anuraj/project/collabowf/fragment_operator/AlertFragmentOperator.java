63543a5a347d553952fccd0142a0992b4374542f
Anuraj R <anuraj.r@sap.com>
2019-05-07 16:06:44 +0530

    AlertFragmentOperator for handle alert screen in the operator

---
+++ app/src/main/java/com/anuraj/project/collabowf/fragment_operator/AlertFragmentOperator.java
@@ -0,0 +1,118 @@
+/*
+ * *
+ *  * Created by Anuraj R (a4anurajr@gmail.com) on 2019
+ *  * Last modified 7/5/19 11:19 AM
+ *
+ */
+package com.anuraj.project.collabowf.fragment_operator;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anuraj.project.collabowf.R;
+import com.anuraj.project.collabowf.SwipeableLayout.EventListParentAdapter;
+import com.anuraj.project.collabowf.model.AlertModel;
+import com.anuraj.project.collabowf.model.EventDates;
+import com.anuraj.project.collabowf.model.EventInformation;
+import com.anuraj.project.collabowf.model.Events;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class AlertFragmentOperator extends Fragment {
+
+    DatabaseReference databaseReference;
+    ProgressDialog progressDialog;
+    View rootView;
+    AlertModel alertModel;
+    RecyclerView event_recycler_view_parent;
+    EventListParentAdapter event_list_parent_adapter;
+    EventInformation eventInformation = new EventInformation();
+    ArrayList<EventDates> eventDatesArrayList;
+
+    public AlertFragmentOperator(){}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.alertfragment, container, false);
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading Data");
+        progressDialog.show();
+        setAlertsDataAdapterAndRecyclerView();
+        return rootView;
+    }
+
+    private void setAlertsDataAdapterAndRecyclerView() {
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("alerts");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                eventDatesArrayList = new ArrayList<>();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    EventDates eventDates = new EventDates();
+                    eventDates.setDate(dataSnapshot.getKey());
+                    ArrayList<Events> eventsArrayList = new ArrayList<>();
+
+                    for (DataSnapshot dataAlert : dataSnapshot.getChildren()) {
+                        alertModel = dataAlert.getValue(AlertModel.class);
+                        Events events = new Events();
+                        if((alertModel.getStatus().equalsIgnoreCase("On Leave")) && (alertModel.getSupervisorseen().equalsIgnoreCase("false"))) {
+                            events.setEventId(alertModel.getId());
+                            events.setEventDate(alertModel.getSelecteddate());
+                            events.setEventAlertDate(eventDates.getDate());
+                            events.setEventOPName(alertModel.getName());
+                            events.setEventStatus(alertModel.getStatus());
+                            events.setEventName(alertModel.getName() + " requested for Leave on " + alertModel.getSelecteddate());
+                        }else if(alertModel.getSupervisorseen().equalsIgnoreCase("false")){
+                            events.setEventId(alertModel.getId());
+                            events.setEventDate(alertModel.getSelecteddate());
+                            events.setEventAlertDate(eventDates.getDate());
+                            events.setEventOPName(alertModel.getName());
+                            events.setEventStatus(alertModel.getStatus());
+                            events.setEventName(alertModel.getName() + " assigned to " + alertModel.getStatus() + " on " + alertModel.getSelecteddate());
+                        }
+                        //to handle the null alert listing
+                        if(events.getEventId()!=null){
+                            eventsArrayList.add(events);
+                        }
+                }
+                    if(!eventsArrayList.isEmpty()){
+                        eventDates.setEventsArrayList(eventsArrayList);
+                        eventDatesArrayList.add(eventDates);
+                    }
+                }
+                eventInformation.setEventsDatesList(eventDatesArrayList);
+                Log.d("Alert",eventInformation.toString());
+                //parent recyclerview
+                event_recycler_view_parent = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+                event_list_parent_adapter = new EventListParentAdapter(eventInformation,getActivity());
+                event_recycler_view_parent.setHasFixedSize(true);
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+                event_recycler_view_parent.setLayoutManager(mLayoutManager);
+                event_recycler_view_parent.setItemAnimator(new DefaultItemAnimator());
+                event_recycler_view_parent.setAdapter(event_list_parent_adapter);
+                progressDialog.dismiss();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                progressDialog.dismiss();
+
+            }
+        });
+    }
+}
