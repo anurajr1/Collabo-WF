b41bf23982e21bd99fc451c7eecb523a73ff3cdd
Anuraj R <anuraj.r@sap.com>
2019-06-06 14:15:51 +0530

    Added the viewpager and tab layout UI

--- app/src/main/java/com/anuraj/project/collabowf/fragment_operator/AlertFragmentOperator.java
+++ app/src/main/java/com/anuraj/project/collabowf/fragment_operator/AlertFragmentOperator.java
@@ -6,124 +6,75 @@
  */
 package com.anuraj.project.collabowf.fragment_operator;
 
-import android.app.ProgressDialog;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.anuraj.project.collabowf.R;
-import com.anuraj.project.collabowf.SwipeableLayout.EventListParentAdapterOperator;
-import com.anuraj.project.collabowf.model.AlertModel;
-import com.anuraj.project.collabowf.model.EventDates;
-import com.anuraj.project.collabowf.model.EventInformation;
-import com.anuraj.project.collabowf.model.Events;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-
-import static com.anuraj.project.collabowf.utils.AppConstants.LOGIN_PREFERENCES;
-
+import java.util.List;
 public class AlertFragmentOperator extends Fragment {
 
-    DatabaseReference databaseReference;
-    ProgressDialog progressDialog;
     View rootView;
-    AlertModel alertModel;
-    RecyclerView event_recycler_view_parent;
-    EventListParentAdapterOperator event_list_parent_adapter;
-    EventInformation eventInformation = new EventInformation();
-    ArrayList<EventDates> eventDatesArrayList;
-
-    SharedPreferences pref;
 
     public AlertFragmentOperator(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.alertfragment, container, false);
-        //shared prefereance insatnce
-        pref = getContext().getSharedPreferences(LOGIN_PREFERENCES, 0); // 0 - for private mode
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setMessage("Loading Data");
-        progressDialog.show();
-        setAlertsDataAdapterAndRecyclerView();
+        rootView = inflater.inflate(R.layout.alertmain_layout, container, false);
+
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) rootView.findViewById(R.id.result_tabs);
+        tabs.setupWithViewPager(viewPager);
+
         return rootView;
     }
 
-    private void setAlertsDataAdapterAndRecyclerView() {
 
-
-        databaseReference = FirebaseDatabase.getInstance().getReference("alerts");
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                eventDatesArrayList = new ArrayList<>();
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    EventDates eventDates = new EventDates();
-                    eventDates.setDate(dataSnapshot.getKey());
-                    ArrayList<Events> eventsArrayList = new ArrayList<>();
-
-                    for (DataSnapshot dataAlert : dataSnapshot.getChildren()) {
-                        alertModel = dataAlert.getValue(AlertModel.class);
-                        Events events = new Events();
-                        if((alertModel.getStatus().equalsIgnoreCase("On Leave"))) {
-                            events.setEventId(alertModel.getId());
-                            events.setEventDate(alertModel.getSelecteddate());
-                            events.setEventAlertDate(eventDates.getDate());
-                            events.setEventOPName(alertModel.getName());
-                            events.setEventStatus(alertModel.getStatus());
-                            events.setEventName("You requested for Leave on " + alertModel.getSelecteddate());
-                            events.setEventSupervisorStatus(alertModel.getSupervisorseen());
-                        }else {
-                            events.setEventId(alertModel.getId());
-                            events.setEventDate(alertModel.getSelecteddate());
-                            events.setEventAlertDate(eventDates.getDate());
-                            events.setEventOPName(alertModel.getName());
-                            events.setEventStatus(alertModel.getStatus());
-                            events.setEventSupervisorStatus(alertModel.getSupervisorseen());
-                            events.setEventName(alertModel.getName() + " assigned to " + alertModel.getStatus() + " on " + alertModel.getSelecteddate());
-                        }
-                        //to handle the null alert listing and filtering of only specific operator related alerts
-                        if(events.getEventId()!=null) {
-                            if (events.getEventId().equalsIgnoreCase(pref.getString("employeeId", null))) {
-                                eventsArrayList.add(events);
-                            }
-                        }
-                }
-                    if(!eventsArrayList.isEmpty()){
-                        eventDates.setEventsArrayList(eventsArrayList);
-                        eventDatesArrayList.add(eventDates);
-                    }
-                }
-                eventInformation.setEventsDatesList(eventDatesArrayList);
-                Log.d("Alert",eventInformation.toString());
-                //parent recyclerview
-                event_recycler_view_parent = (RecyclerView) rootView.findViewById(R.id.recyclerView);
-                event_list_parent_adapter = new EventListParentAdapterOperator(eventInformation,getActivity());
-                event_recycler_view_parent.setHasFixedSize(true);
-                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
-                event_recycler_view_parent.setLayoutManager(mLayoutManager);
-                event_recycler_view_parent.setItemAnimator(new DefaultItemAnimator());
-                event_recycler_view_parent.setAdapter(event_list_parent_adapter);
-                progressDialog.dismiss();
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-                progressDialog.dismiss();
-
-            }
-        });
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new MyAlertFragment(), "My Alerts");
+        adapter.addFragment(new GeneralAlertFragment(), "General Alerts");
+        viewPager.setAdapter(adapter);
     }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
